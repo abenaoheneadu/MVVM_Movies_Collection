ad157b78b7bfe18a702007050c51c7dfd23e8332
Field Employee <fieldemployee@Fields-MBP.hsd1.ga.comcast.net>
2021-07-11 15:38:18 -0400

    got api to work

--- app/src/main/java/com/dynnamicdevz/mymoviescollectionapp/view/activity/MainActivity.kt
+++ app/src/main/java/com/dynnamicdevz/mymoviescollectionapp/view/activity/MainActivity.kt
@@ -2,15 +2,32 @@ package com.dynnamicdevz.mymoviescollectionapp.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.fragment.app.viewModels
 import com.dynnamicdevz.mymoviescollectionapp.R
+import com.dynnamicdevz.mymoviescollectionapp.databinding.ActivityMainBinding
+import com.dynnamicdevz.mymoviescollectionapp.view.adapter.MoviesAdapter
 import com.dynnamicdevz.mymoviescollectionapp.view.fragment.DisplayMoviesFragment
+import com.dynnamicdevz.mymoviescollectionapp.viewmodel.MoviesViewModel
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var displayMoviesFragment: DisplayMoviesFragment
+    //private lateinit var displayMoviesFragment: DisplayMoviesFragment
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MoviesViewModel by viewModels()
+    private val adapter = MoviesAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        displayMoviesFragment = supportFragmentManager.findFragmentById(R.id.display_movies_fragment) as DisplayMoviesFragment
+       // displayMoviesFragment = supportFragmentManager.findFragmentById(R.id.display_movies_fragment) as DisplayMoviesFragment
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.movieSortedRecyclerview.adapter = adapter
+        viewModel.moviesLiveData.observe(this, {
+            adapter.list = it
+            Log.d("TAG_X", "Data retrieved ${it.size}")
+        })
+        viewModel.getAllPopularMovies()
     }
 }
--- app/src/main/java/com/dynnamicdevz/mymoviescollectionapp/view/fragment/DisplayMoviesFragment.kt
+++ app/src/main/java/com/dynnamicdevz/mymoviescollectionapp/view/fragment/DisplayMoviesFragment.kt
@@ -5,8 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dynnamicdevz.mymoviescollectionapp.databinding.DisplayMoviesFragmentBinding
 import com.dynnamicdevz.mymoviescollectionapp.view.adapter.MoviesAdapter
+import com.dynnamicdevz.mymoviescollectionapp.viewmodel.MoviesViewModel
+
 
 class DisplayMoviesFragment:Fragment() {
     private lateinit var binding: DisplayMoviesFragmentBinding
@@ -17,13 +21,23 @@ class DisplayMoviesFragment:Fragment() {
     ): View {
         binding = DisplayMoviesFragmentBinding.inflate(inflater, container, false)
         return binding.root
+
     }
+    private val viewModel: MoviesViewModel by viewModels()
     private val adapter = MoviesAdapter()
+    //var manager = LinearLayoutManager(this.context)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //binding.movieRecyclerview.layoutManager = manager
         binding.movieRecyclerview.adapter = adapter
+        viewModel.moviesLiveData.observe(viewLifecycleOwner, {
+            adapter.list = it
+        })
+        viewModel.getAllPopularMovies()
+
     }
+
     // do you see this
 
 //    fun updateTeams(list: List<Team>){
